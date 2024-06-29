2cc9ae269c23b3d9d01a12b281b70cb5687f7d13
Ahmad Rizal <pavaroid@gmail.com>
2024-06-29 20:08:36 +0700

    menambahkan react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Reoutes,
+  Link,
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Reach from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
